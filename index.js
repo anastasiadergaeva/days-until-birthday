e8c37a85a1a6cbfea4f6c6beae91db2f362adfeb
anastasiadergaeva <adergaeva@gmail.com>
2023-11-02 23:29:51 +0300

    fix declensions of a day word in ru

--- index.js
+++ index.js
@@ -13,15 +13,24 @@ function getDaysUntilBirthday() {
     } else {
         const parsedDate = Date.parse(currentDate);
         const userBirthday = Date.parse(dateFromInputValue);
-
         let datesDiff = userBirthday - parsedDate;
         let datesDiffInDays = Math.ceil(datesDiff / 1000 / 3600 / 24);
+
+        let declensionOfADayWord = "";
+        if (datesDiffInDays === 1 || datesDiffInDays === 21 || datesDiffInDays === 31) {
+            declensionOfADayWord = "день";
+        } else if (datesDiffInDays === 2 || datesDiffInDays === 3 || datesDiffInDays === 4 || datesDiffInDays === 22 || datesDiffInDays === 23 || datesDiffInDays === 24) {
+            declensionOfADayWord = "дня";
+        } else {
+            declensionOfADayWord = "дней";
+        }
+
         if (datesDiffInDays < 0) {
             resultParagraph.innerHTML = 'Ваш день рождения уже прошёл!<br/>Попробуйте в новом году!';
         } else if (datesDiffInDays === 0) {
             resultParagraph.textContent = 'Вы родились сегодня! С днём рождения!';
         } else {
-            resultParagraph.textContent = `Ваш день рождения наступит через ${datesDiffInDays} дней!`;
+            resultParagraph.textContent = `Ваш день рождения наступит через ${datesDiffInDays} ${declensionOfADayWord}!`;
         }
     }
 }
