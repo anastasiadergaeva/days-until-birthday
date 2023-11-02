404dae31cbae4274dc661cf1018bf49bda23c460
anastasiadergaeva <adergaeva@gmail.com>
2023-11-02 22:46:24 +0300

    fix bugs

--- index.js
+++ index.js
@@ -1,47 +1,29 @@
-const dateFromInput = document.querySelector('#inputdate').value;
+const dateFromInput = document.querySelector('#inputdate');
 const errorMessage = document.getElementById('errormessage');
 const resultButton = document.querySelector('.result-button');
 const resultParagraph = document.querySelector('.main__result');
-
-function checkEmptyInput() {
-    errorMessage.innerHTML = '';
-    if (dateFromInput.value == '') {
-        errorMessage.textContent = 'Введите свою дату рождения';
-    }
-}
+const currentDate = new Date();
 
 function getDaysUntilBirthday() {
-    const currentDate = new Date();
-    const currentYear = currentDate.getFullYear();
-    const userBirthday = new Date(dateFromInput);
-    const userBirthdateYear = userBirthday.getFullYear();
-    const userBirthdateMonth = userBirthday.getMonth();
-    const userBirthdateDay = userBirthday.getDay();
-    const birthDate = new Date(userBirthdateYear, userBirthdateMonth, userBirthdateDay);
-    if (birthDate.getFullYear() >= currentDate.getFullYear()) {
-        let dateUntilBirthday;
-        dateUntilBirthday = Math.ceil((birthDate.getTime() - currentDate.getTime()) / (1000 * 60 * 60 * 24));
+    const dateFromInputValue = dateFromInput.value;
+    errorMessage.innerHTML = '';
+    resultParagraph.innerHTML = '';
+    if (dateFromInput.value == '') {
+        errorMessage.textContent = 'Введите свою дату рождения';
     } else {
-        birthDate.setFullYear(currentYear + 1);
-        dateUntilBirthday = Math.ceil((birthDate.getTime() - currentDate.getTime()) / (1000 * 60 * 60 * 24));
+        const parsedDate = Date.parse(currentDate);
+        const userBirthday = Date.parse(dateFromInputValue);
+
+        let datesDiff = userBirthday - parsedDate;
+        let datesDiffInDays = Math.floor(datesDiff / 1000 / 3600 / 24);
+        if (datesDiffInDays < 0) {
+            resultParagraph.textContent = 'Ваш день рождения уже прошёл!';
+        } else if (datesDiffInDays === 0) {
+            resultParagraph.textContent = 'Вы родились сегодня! С днём рождения!';
+        } else {
+            resultParagraph.textContent = `Ваш день рождения наступит через ${datesDiffInDays} дней!`;
+        }
     }
-    console.log(dateUntilBirthday);
-    console.log(birthDate);
 }
-
-function calculateDaysUntilBirthday() {
-    let declensionOfADayWord = "";
-    if (dateUntilBirthday === 1) {
-        declensionOfADayWord = "день";
-    } else if (dateUntilBirthday < 5) {
-        declensionOfADayWord = "дня";
-    } else {
-        declensionOfADayWord = "дней";
-    }
-
-    resultParagraph.innerHTML = `До Вашего дня рождения осталось ${dateUntilBirthday} ${declensionOfADayWord}`;
-}
-
-console.log(getDaysUntilBirthday());
 
 resultButton.addEventListener('click', getDaysUntilBirthday);
